8151aa701165cff8e320197f28c00a87d8fe5bc7
kunal-agr <kunalkagrawal2004@gmail.com>
2025-12-19 19:30:28 +0530

    Fixed DBConnection

--- src/main/java/com/kagrawal/util/DBConnection.java
+++ src/main/java/com/kagrawal/util/DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = "jdbc:postgresql://localhost:5432/mvcdetsdb";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "postgres";
+    private static final String PASSWORD = "password";
 
     static {
         try {
