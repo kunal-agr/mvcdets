1fce063f5e3ad00df5ad2298b1144037bcb34ec0
kunal-agr <kunalkagrawal2004@gmail.com>
2025-12-19 19:15:19 +0530

    Add Postgresql JDBC connection utility

---
+++ src/main/java/com/kagrawal/util/DBConnection.java
@@ -0,0 +1,23 @@
+package com.kagrawal.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:postgresql://localhost:5432/mvcdetsdb";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "postgres";
+
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
