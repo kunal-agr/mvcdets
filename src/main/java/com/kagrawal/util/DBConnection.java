e4edd00f585d0293b85ef8fa3b966f8ec9a495d6
kunal-agr <kunalkagrawal2004@gmail.com>
2026-01-16 12:05:41 +0530

    Daily Expense Tracker ready for Deployment

--- src/main/java/com/kagrawal/util/DBConnection.java
+++ src/main/java/com/kagrawal/util/DBConnection.java
@@ -10,7 +10,7 @@ public class DBConnection {
 //    private static final String PASSWORD = "password";
     private static final String URL = System.getenv("DB_URL");
     private static final String USER = System.getenv("DB_USER");
-    private static final String PASSWORD = System.getenv("DB_PASSWORD");
+    private static final String PASSWORD = System.getenv("DB_PASS");
 
     static {
         try {
