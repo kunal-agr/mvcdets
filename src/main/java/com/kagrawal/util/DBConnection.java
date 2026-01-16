3c270ed84f5bc02cdacb82c072fd0bdc76aa89fa
kunal-agr <kunalkagrawal2004@gmail.com>
2026-01-16 11:53:40 +0530

    Daily Expense Tracker ready for Deployment

--- src/main/java/com/kagrawal/util/DBConnection.java
+++ src/main/java/com/kagrawal/util/DBConnection.java
@@ -5,9 +5,12 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:postgresql://localhost:5432/mvcdetsdb";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "password";
+//    private static final String URL = "jdbc:postgresql://localhost:5432/mvcdetsdb";
+//    private static final String USER = "postgres";
+//    private static final String PASSWORD = "password";
+    private static final String URL = System.getenv("DB_URL");
+    private static final String USER = System.getenv("DB_USER");
+    private static final String PASSWORD = System.getenv("DB_PASSWORD");
 
     static {
         try {
